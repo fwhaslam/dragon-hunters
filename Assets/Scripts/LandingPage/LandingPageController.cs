262c81c5edb3ea95f43f6e9fe61cad58e8f985c7
fwhaslam <fwhaslam@gmail.com>
2016-07-06 09:23:11 -0700

    Metas for landing page na tools.

---
+++ Assets/Scripts/LandingPage/LandingPageController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LandingPageController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		//List<string> filenames = FileUtil.GetAllPaths ();
+		//foreach ( string filename in filenames ) {
+		//	print (" FILENAME = " + filename);
+		//}
+
+		FileUtil.visitAssetFiles ((string filepath) => {
+			if (filepath.EndsWith(".tsv")) print("FILE="+filepath);
+		});
+
+		//print (" FILENAMES = " + filenames);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
